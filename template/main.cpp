33e0e07ca5546cbfd64b602b845f82d802c4c787
Irk Lost <284513799+irklost@users.noreply.github.com>
2026-05-15 01:03:40 -0400

    feat: add template dir

---
+++ template/main.cpp
@@ -0,0 +1,67 @@
+// {{{
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <array>
+#include <deque>
+#include <list>
+#include <forward_list>
+#include <stack>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <bitset>
+#include <string>
+#include <string_view>
+#include <regex>
+#include <algorithm>
+#include <numeric>
+#include <functional>
+#include <iterator>
+#include <cmath>
+#include <cstdlib>
+#include <climits>
+#include <cfloat>
+#include <complex>
+#include <random>
+#include <bit>
+#include <utility>
+#include <tuple>
+#include <optional>
+#include <variant>
+#include <any>
+#include <memory>
+#include <type_traits>
+#include <chrono>
+#include <cassert>
+#include <stdexcept>
+
+#ifdef LOCAL
+#include "dbg.h"
+#else
+#define dbg(...)
+#endif
+// }}}
+
+// #include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) { solve(); }
+    return 0;
+}
